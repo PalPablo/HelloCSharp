7fef97ed96cb08216135dee181278e16c4088713
Pavel Zykin <prime.87.pz@gmail.com>
2023-03-09 21:26:06 +0300

    add random

---
+++ Еxample003_Sum/Program.cs
@@ -0,0 +1,20 @@
+﻿//int numberA = 3;
+//int numberВ = 5;
+//Console.WriteLine(numberA + numberВ);
+
+//int numberA = 31;
+//int numberВ = 5;
+//int result = numberA + numberВ;
+//Console.WriteLine(result);
+
+//int numberA = new Random().Next(1,10);
+//int numberB = new Random().Next(1,10);
+//int result = numberA + numberB;
+//Console.WriteLine(result);
+
+int numberA = new Random().Next(1,10);
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10);
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
