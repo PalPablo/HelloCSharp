4b318dba6bd5958df4a66e1724ce70d8729ffece
Pavel Zykin <prime.87.pz@gmail.com>
2023-04-27 21:11:35 +0300

    Methods

--- Example012_Methods/Program.cs
+++ Example012_Methods/Program.cs
@@ -48,19 +48,145 @@
 
 // Четвёртая группа методов. Что-то принимает (аргументы, данные). И что-то возвращает для дальнейшей работы.
 
-string Method4(int count, string text)
+// string Method4(int count, string text)
+// {
+//     int i = 0;
+//     string result = String.Empty;
+
+//     while (i < count)
+//     {
+//         result = result + text;
+//         i++;
+//     }
+//     return result;
+// }
+
+// string res = Method4(10, "p");
+// Console.WriteLine(res);
+
+// То же самое с циклом for.
+
+// string Method4(int count, string text)
+// {
+//     string result = String.Empty;
+//     for (int i = 0; i < count; i++)
+//     {
+//         result = result + text;
+//     }
+//     return result;
+// }
+
+// string res = Method4(10, "p");
+// Console.WriteLine(res);
+
+// Таблица умножения
+
+// for (int i = 2; i <= 10; i++)
+// {
+// for (int j = 2; j <= 10; j++)
+// {
+// Console.WriteLine($"{i} * {j} = {i * j}");
+// }
+// Console.WriteLine();
+// }
+
+
+// Задача. Дан текст. В нём нужно все пробелы заменить чёрточками, маленькие буквы «к»
+// заменить большими «К», а маленькие «с» заменить на большие «С».
+
+// string text = "— Я думаю, — сказал князь, улыбаясь, — что,"
+// + "ежели бы вас послали вместо нашего милого Винценгероде,"
+// + "вы бы взяли приступом согласие прусского короля."
+// + "Вы так красноречивы. Вы дадите мне чаю?";
+
+// // string s = “qwerty”
+// // 012345
+
+// // s[3] // r
+
+// string Replace(string text, char oldValue, char newValue)
+// {
+// string result = String.Empty;
+// int length = text.Length;
+// for (int i = 0; i < length; i++)
+// {
+// if(text[i] == oldValue) result = result + $"{newValue}";
+// else result = result + $"{text[i]}";
+// }
+// return result;
+// }
+// string newText = Replace(text, ' ', '|');
+// Console.WriteLine(newText);
+
+// Console.WriteLine();
+// newText = Replace(newText, 'к', 'К');
+// Console.WriteLine(newText);
+
+// Console.WriteLine();
+// newText = Replace(newText, 'с', 'С');
+// Console.WriteLine(newText);
+
+
+//  Написать метод, который будет упорядочивать наш массив от меньшего числа к большему.
+
+// int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+// void PrintArray(int[] array)
+// {
+//     int count = array.Length;
+//     for (int i = 0; i < count; i++)
+//     {
+//         Console.Write($"{array[i]}");
+//     }
+//     Console.WriteLine();
+// }
+// void SelectionSort(int[] array)
+// {
+//     for (int i = 0; i < array.Length - 1; i++)
+//     {
+//         int minPosition = i;
+//         for (int j = i + 1; j < array.Length; j++)
+//         {
+//             if (array[j] < array[minPosition])
+//             {
+//                 minPosition = j;
+//             }
+//         }
+//         int temporary = array[i];
+//         array[i] = array[minPosition];
+//         array[minPosition] = temporary;
+//     }
+// }
+// PrintArray(arr);
+// SelectionSort(arr);
+// PrintArray(arr);
+
+
+// Написать метод, который будет упорядочивать наш массив от большего числа к меньшему.
+
+int[] arr = { 1, 5, 4, 3, 2, 6, 7, 1, 1 };
+void PrintArray(int[] array)
 {
-    int i = 0;
-    string result = String.Empty;
-
-    while (i < count)
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
     {
-        result = result + text;
-        i++;
+        Console.Write($"{array[i]}");
     }
-    return result;
+    Console.WriteLine();
 }
-
-string res = Method4(10, "p");
-Console.WriteLine(res);
-
+void SelectionSort(int[] array)
+{
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        int maxPosition = i;
+        for (int j = i + 1; j < array.Length; j++)
+        {
+            if (array[j] > array[maxPosition]) maxPosition = j;
+        }
+        int temporary = array[i];
+        array[i] = array[maxPosition];
+        array[maxPosition] = temporary;
+    }
+}
+PrintArray(arr);
+SelectionSort(arr);
+PrintArray(arr);
